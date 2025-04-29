4cd61757b640b5eea23d5d90cf2aa7d079cd3c04
ctrindadedev <caiomedtrindade@gmail.com>
2025-04-29 10:05:04 -0300

    estruturação da primeira atividade proposta

---
+++ activities/banking_system/classes.h
@@ -0,0 +1,15 @@
+#ifndef _CLASSES_H // Evita a redefinicao dos membros do arquivo
+#define _CLASSES_H // Inicio da definicao de _CLASSES_H
+
+class Cliente {
+
+
+};
+
+class ContaBancaria {
+
+
+};
+
+#endif // Fim da definicao de _CLASSES_H
+
