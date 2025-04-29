0f1d8b58b49e2722d34efd92e226c51911881fd5
ctrindadedev <caiomedtrindade@gmail.com>
2025-04-29 10:05:51 -0300

    correções no conteudo de modularização

--- content/modularization_compilation/conv.cpp
+++ content/modularization_compilation/conv.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 #include "conv.h"
-
-int f2c(float tempk({return tempk - 32 / 18});
+int main() {
+int opcao;
+cout << "Conversor de temperatura" << endl;
+cout << "(1) Celsius -> Fahrenheit" << endl;
+cout << "(2) Fahrenheit -> Celsius" << endl;
+cout << "Digite sua opcao: ";
+cin >> opcao;
+switch(opcao) {
+float temp;
+cout << "Digite a temperatura: ";
+cin >> temp;
+float conv;
+case 1:
+conv = celsius2fahrenheit(temp);
+cout << temp << "oC = " << conv << "oF" << endl;
+break;
+case 2:
+conv = fahrenheit2celsius(temp);
+cout << temp << "oF = " << conv << "oC" << endl;
+break;
+default:
+cout << "Opcao invalida" << endl;
+}
+return 0;
+}
--- content/modularization_compilation/conv.h
+++ content/modularization_compilation/conv.h
@@ -1,6 +1,16 @@
-#ifndef _CONV_H
-#define _CONV_H
+#ifndef _CONV_H // Evita a redefinicao dos membros do arquivo
+#define _CONV_H // Inicio da definicao de NOME_DO_ARQUIVO_H
 
-float f2c(temp);
+// Conversao de temperatura em escala Celsius para Fahrenheit
+float celsius2fahrenheit(float temp) {
+return temp * 1.8 + 32;
+}
+// Conversao de temperatura em escala Fahrenheit para Celsius
+float fahrenheit2celsius(float temp) {
+return (temp - 32) / 1.8;
+}
 
-#endif
+#endif // Fim da definicao de _CONV_H
+
+
+
